32e5638e36971b55dda9222247d2ca5c549009e6
Alex Yong <alexjyong@gmail.com>
2025-05-01 10:47:51 -0400

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -1,43 +1,44 @@
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.start = start;
-exports.onRequest = onRequest;
-exports.sendResponse = sendResponse;
-exports.stop = stop;
-
-var _exec = require('cordova/exec');
-
-var _exec2 = _interopRequireDefault(_exec);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var exec = require('cordova/exec');
 
 var WEBSERVER_CLASS = 'Webserver';
 var START_FUNCTION = 'start';
 var ONREQUEST_FUNCTION = 'onRequest';
-var SENDRESPONSE_FUNCION = 'sendResponse';
+var SENDRESPONSE_FUNCTION = 'sendResponse';
 var STOP_FUNCTION = 'stop';
+var PUSHFRAME_FUNCTION = 'pushFrame';
 
 function start(success_callback, error_callback, port) {
   var params = [];
   if (port) {
     params.push(port);
   }
-  (0, _exec2.default)(success_callback, error_callback, WEBSERVER_CLASS, START_FUNCTION, params);
+  exec(success_callback, error_callback, WEBSERVER_CLASS, START_FUNCTION, params);
 }
 
 function onRequest(success_callback) {
-  (0, _exec2.default)(success_callback, function (error) {
+  exec(success_callback, function (error) {
     console.error(error);
   }, WEBSERVER_CLASS, ONREQUEST_FUNCTION, []);
 }
 
 function sendResponse(requestId, params, success_callback, error_callback) {
-  (0, _exec2.default)(success_callback, error_callback, WEBSERVER_CLASS, SENDRESPONSE_FUNCION, [requestId, params]);
+  exec(success_callback, error_callback, WEBSERVER_CLASS, SENDRESPONSE_FUNCTION, [requestId, params]);
 }
 
 function stop(success_callback, error_callback) {
-  (0, _exec2.default)(success_callback, error_callback, WEBSERVER_CLASS, STOP_FUNCTION, []);
+  exec(success_callback, error_callback, WEBSERVER_CLASS, STOP_FUNCTION, []);
 }
+
+function pushFrame(base64Jpeg, success_callback, error_callback) {
+  exec(success_callback, error_callback, WEBSERVER_CLASS, PUSHFRAME_FUNCTION, [base64Jpeg]);
+}
+
+module.exports = {
+  start: start,
+  onRequest: onRequest,
+  sendResponse: sendResponse,
+  stop: stop,
+  pushFrame: pushFrame
+};
